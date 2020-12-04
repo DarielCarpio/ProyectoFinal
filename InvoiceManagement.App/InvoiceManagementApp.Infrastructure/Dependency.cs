9b306cda951fcc24ca6322db1d580ad8c182c4c9
DarielCarpio <72769184+DarielCarpio@users.noreply.github.com>
2020-12-04 15:39:59 -0400

    Proyectofinaall
    Mi proyectoo

---
+++ InvoiceManagement.App/InvoiceManagementApp.Infrastructure/Dependency.cs
@@ -0,0 +1,34 @@
+﻿using InvoiceManagement.App.Infrastructure.Data;
+using InvoiceManagement.App.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceManagementApp.Infrastructure
+{
+    public static class Dependency
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+            return services;
+        }
+            
+    }
+}
---
+++ InvoiceManagement.App/InvoiceManagementApp.Infrastructure/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceManagement.App.Infrastructure.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
